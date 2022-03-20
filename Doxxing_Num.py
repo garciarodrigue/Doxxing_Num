54d83321f6fa2fc0bc012d41e153a702f981c892
TENSHI101 <48865498+garciarodrigue@users.noreply.github.com>
2022-03-19 21:19:57 -0600

    Add files via upload
    Vercion 1.0 
    La api dejara de funcionar cuando esta este muy quemada

---
+++ Doxxing_Num.py
@@ -0,0 +1,29 @@
+import requests
+import pyfiglet
+import os
+BLACK = '\033[30m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
+RESET = '\033[39m'
+os.system("clear")
+baner= pyfiglet.figlet_format("Doxxing\nNum")
+print(RED+baner)
+print(BLUE+"\nby x T3nshi")
+print("Legion Anonox")
+print(YELLOW+"escribe el numero de telefono junto\ncon el codigo de pais, ejemplo: +50234563428\n")
+# Información
+
+api_key = 'b55cc727b39065ab10c752064013930a'
+number = int(input(RED+"Numero: "+RESET))
+
+data = requests.get("http://apilayer.net/api/validate?access_key=%s&number=%s&country_code&format=1" % (api_key, number))
+
+for key, value in data.json().items():
+
+    print("%s: %s" % (key, value))
+
